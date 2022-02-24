1eede8b8e60f59b6ef9a2f7b93f8a700098be763
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-02-24 19:19:46 +0000

    innitial commit

---
+++ math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by Samuel Webster
+# Created on February 2022
+# This is the "Hello, World!" program with proper style
+
+
+def main():
+
+    print("If a rectangle has the dimensions: 5cm x 3cm")
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
