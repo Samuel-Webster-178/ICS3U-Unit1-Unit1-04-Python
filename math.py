d59916a8c12a00cc5298a9dd12a02548eaa594f5
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-05-01 21:13:40 +0000

    fix2

--- math.py
+++ math.py
@@ -2,14 +2,14 @@
 
 # Created by Samuel Webster
 # Created on February 2022
-# Calculates rectangle perimeter with dimmensions of 5c x 3cm
+# Calculates rectangle perimeter and area with dimensions of 5c x 3cm
 
 
 def main():
-    # I Calculate the perimeter of a rectangle with dimmensions of 5c x 3cm
+    # I Calculate the perimeter and area of a rectangle with dimensions of 5c x 3cm
     print("If a rectangle has the dimensions: 5cm x 3cm")
-    print("Area is {}cm².".format(3 * 5))
-    print("\nPerimeter is {}cm.".format(2 * (3 + 5)))
+    print("\nArea is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
     print("\nDone.")
 
 
